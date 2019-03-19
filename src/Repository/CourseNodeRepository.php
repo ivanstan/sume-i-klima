bbbfb18ab496300ef948aa664eeb859c1bee2e4b
Ivan Stanojevic <ivan@byteout.com>
2019-03-19 11:11:16 +0100

    moved node tables to use single inheritance

--- src/Repository/CourseNodeRepository.php
+++ src/Repository/CourseNodeRepository.php
@@ -4,12 +4,7 @@ namespace App\Repository;
 
 use App\Entity\AbstractCourseNode;
 use App\Entity\Course;
-use App\Entity\CourseNodeQuiz;
-use App\Entity\QuizAnswer;
-use App\Entity\QuizQuestion;
-use App\Entity\QuizQuestionAnswer;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\Query\Expr\Join;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class CourseNodeRepository extends ServiceEntityRepository
@@ -23,21 +18,12 @@ class CourseNodeRepository extends ServiceEntityRepository
     {
         $builder = $this->createQueryBuilder('node');
 
-        $builder->select('node', 'course'
-//            ,
-//            'quiz', 'quiz_question', 'answers', 'answer'
-        )
+        $builder->select('node', 'course')
             ->innerJoin('node.course', 'course')
-//            ->leftJoin(CourseNodeQuiz::class, 'quiz', Join::WITH, 'quiz.id = node.id')
-//            ->leftJoin(QuizQuestion::class, 'quiz_question', Join::WITH, 'quiz_question.quiz = quiz.id')
-//            ->leftJoin('quiz_question.answers', 'answers')
-//            ->leftJoin('answers.answer', 'answer')
             ->where('course.id = :course')->setParameter('course', $course)
             ->andWhere('node.parent IS NULL')
-//            ->andWhere('node INSTANCE OF App\Entity\CourseNodeEnvelope')
+            ->andWhere('node INSTANCE OF App\Entity\CourseNodeEnvelope')
         ;
-
-        echo "<pre>"; print_r($builder->getQuery()->getSQL()); echo "</pre>"; die();
 
         return $builder->getQuery()->getResult();
     }
